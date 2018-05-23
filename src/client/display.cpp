2e443b988f513baff00a6d9214a934bcedf009f6
drumsetmonkey <drumsetmonkey@gmail.com>
2018-05-23 01:12:26 -0400

    Fixed tabs

--- src/client/display.cpp
+++ src/client/display.cpp
@@ -7,11 +7,11 @@ Display::Display(std::string title, int width, int height)
     this->title = title;
     this->screenSize = vec2<int>(width, height);
 
-	SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
-	// Select an OpenGL 2.1 profile.
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    // Select an OpenGL 2.1 profile.
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 
     window = nullptr;
     context = nullptr;
@@ -24,10 +24,10 @@ Display::Display()
 
 void Display::create()
 {
-	window = SDL_CreateWindow(title.c_str(),
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		screenSize.x, screenSize.y,
-		SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(title.c_str(),
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        screenSize.x, screenSize.y,
+        SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 
     context = SDL_GL_CreateContext(window);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -97,33 +97,33 @@ public:
 
 static void stackDump (lua_State *L)
 {
-	int i;
-	int top = lua_gettop(L);
-	for (i = 1; i <= top; i++) {  /* repeat for each level */
-		int t = lua_type(L, i);
-		switch (t) {
+    int i;
+    int top = lua_gettop(L);
+    for (i = 1; i <= top; i++) {  /* repeat for each level */
+        int t = lua_type(L, i);
+        switch (t) {
 
-		case LUA_TSTRING:  /* strings */
-			printf("`%s'", lua_tostring(L, i));
-			break;
+        case LUA_TSTRING:  /* strings */
+            printf("`%s'", lua_tostring(L, i));
+            break;
 
-		case LUA_TBOOLEAN:  /* booleans */
-			printf(lua_toboolean(L, i) ? "true" : "false");
-			break;
+        case LUA_TBOOLEAN:  /* booleans */
+            printf(lua_toboolean(L, i) ? "true" : "false");
+            break;
 
-		case LUA_TNUMBER:  /* numbers */
-			printf("%g", lua_tonumber(L, i));
-			break;
+        case LUA_TNUMBER:  /* numbers */
+            printf("%g", lua_tonumber(L, i));
+            break;
 
-		default:  /* other values */
-			printf("%s", lua_typename(L, t));
-			break;
+        default:  /* other values */
+            printf("%s", lua_typename(L, t));
+            break;
 
-		}
-		printf("  ");  /* put a separator */
-	}
-	
-	printf("\n");  /* end the listing */
+        }
+        printf("  ");  /* put a separator */
+    }
+    
+    printf("\n");  /* end the listing */
 }
 
 static World w;
@@ -138,27 +138,27 @@ static int create_block(lua_State *L)
     bool tmpb;
     int tmpf;
 
-	for (lua_pushnil(L); lua_next(L, -2) != 0; lua_pop(L, 1)) {
-		name = lua_tostring(L, -2);
-		int t = lua_type(L, -1);
-		switch(t) {
-		case LUA_TSTRING:
-			tmps = lua_tostring(L, -1);
-			break;
-		case LUA_TBOOLEAN:
-			tmpb = lua_toboolean(L, -1);
-			break;
-		case LUA_TNUMBER:
-			tmpn = lua_tonumber(L, -1);
-			break;
-		case LUA_TFUNCTION:
-			tmpf = luaL_ref(L, LUA_REGISTRYINDEX);
-			lua_pushvalue(L, 1);
-			break;
-		default:
-			std::cout << lua_typename(L, t);
-		}
-		
+    for (lua_pushnil(L); lua_next(L, -2) != 0; lua_pop(L, 1)) {
+        name = lua_tostring(L, -2);
+        int t = lua_type(L, -1);
+        switch(t) {
+        case LUA_TSTRING:
+            tmps = lua_tostring(L, -1);
+            break;
+        case LUA_TBOOLEAN:
+            tmpb = lua_toboolean(L, -1);
+            break;
+        case LUA_TNUMBER:
+            tmpn = lua_tonumber(L, -1);
+            break;
+        case LUA_TFUNCTION:
+            tmpf = luaL_ref(L, LUA_REGISTRYINDEX);
+            lua_pushvalue(L, 1);
+            break;
+        default:
+            std::cout << lua_typename(L, t);
+        }
+        
         if (name == "name") {
            b.setName(tmps);
         } else if (name == "id") {
@@ -172,7 +172,7 @@ static int create_block(lua_State *L)
         } else {
             (void)tmpb;
         }
-	}
+    }
     
     w.addBlock(b);
     return 0;
@@ -180,70 +180,70 @@ static int create_block(lua_State *L)
 
 int main()
 {
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
 
-	lua_register(L, "create_block", create_block);
+    lua_register(L, "create_block", create_block);
 
-	luaL_loadfile(L, "scripts/load.lua");
-	lua_pcall(L, 0, 0, 0);
+    luaL_loadfile(L, "scripts/load.lua");
+    lua_pcall(L, 0, 0, 0);
 
-	//lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
-	//lua_pcall(L, 0, 0, 0);
+    //lua_rawgeti(L, LUA_REGISTRYINDEX, ref);
+    //lua_pcall(L, 0, 0, 0);
 
     stackDump(L);
     
     w.createLuaInstance(L);
     w.printBlocks();
 
-	lua_close(L);
+    lua_close(L);
 
     Game *c = new Client();
     c->init();
 
-	return 0;
+    return 0;
 }
 
 /*int main(int argc, char** argv)
 {
-	lua_State *L = luaL_newstate();
-	luaL_openlibs(L);
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
 
-	auto n = atoi(argv[1]);
+    auto n = atoi(argv[1]);
 
-	int status = luaL_loadfile(L, "hello.lua");
-	
-	int ret = lua_pcall(L, 0, 0, 0); // execute once to assign functions
-	if (ret != 0) {
-		std::cerr << lua_tostring(L, -1);
-		return -1;
-	}
+    int status = luaL_loadfile(L, "hello.lua");
+    
+    int ret = lua_pcall(L, 0, 0, 0); // execute once to assign functions
+    if (ret != 0) {
+        std::cerr << lua_tostring(L, -1);
+        return -1;
+    }
 
-	lua_getglobal(L, "fact"); // function to be called
-	lua_pushnumber(L, n); // push argument
-	stackDump(L);
-	stackDump(L);
-	
-	if (lua_pcall(L, 1, 1, 0) != 0) { // one arg, one return value
-		std::cerr << lua_tostring(L, -1);
-		return -1;
-	}
+    lua_getglobal(L, "fact"); // function to be called
+    lua_pushnumber(L, n); // push argument
+    stackDump(L);
+    stackDump(L);
+    
+    if (lua_pcall(L, 1, 1, 0) != 0) { // one arg, one return value
+        std::cerr << lua_tostring(L, -1);
+        return -1;
+    }
 
-	auto res = lua_tonumber(L, -1);
-	lua_pop(L, 1); // pop returned value
-	std::cout << "Fact: " << res << std::endl;
-	
-	lua_getglobal(L, "Coins");
-	if (lua_istable(L, -1)) {
-		std::cout << "IS TABLE" << std::endl;
-		lua_pushnumber(L, 1);
-		lua_gettable(L, -1);
-		stackDump(L);
-	}
-	stackDump(L);
+    auto res = lua_tonumber(L, -1);
+    lua_pop(L, 1); // pop returned value
+    std::cout << "Fact: " << res << std::endl;
+    
+    lua_getglobal(L, "Coins");
+    if (lua_istable(L, -1)) {
+        std::cout << "IS TABLE" << std::endl;
+        lua_pushnumber(L, 1);
+        lua_gettable(L, -1);
+        stackDump(L);
+    }
+    stackDump(L);
 
-	lua_close(L);
+    lua_close(L);
 
-	return 0;
+    return 0;
 }*/
 
