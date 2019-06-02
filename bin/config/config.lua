122fc830b5b5ac17570766639e6067bed5d2f880
Andy Belle-Isle <drumsetmonkey@gmail.com>
2019-06-02 02:41:22 -0400

    Added configuration file

---
+++ bin/config/config.lua
@@ -0,0 +1,17 @@
+game = {
+    title = "loxel",
+    monitor = {
+        width = 1280,
+        height = 720,
+        fullscreen = false,
+    },
+
+    input = {
+        keyboard = {
+            walk_forward = "w",
+            walk_backward = "s",
+            strafe_left = "a",
+            strage_right = "d",
+        }
+    }
+}
